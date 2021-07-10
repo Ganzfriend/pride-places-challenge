d48fd170618ff7613d077c3f33b4a2bac9f36655
Melissa Ganzfried <melissa.l.ganzfried@biola.edu>
2021-07-10 13:35:51 +0200

    box shadow style

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -16,16 +16,20 @@ const styles = ({palette, breakpoint, spacing}) => (
 
     },
     post: {
-      '&:hover': {
-        cursor: 'pointer',
-        boxShadow: '3px 4px 4px 5px gray',
-      },
       border: '1px solid white',
       borderRadius: 10,
       boxShadow: '2px 1px 4px 1px gray',
       marginTop: spacing(3),
       marginBottom: spacing(3),
       padding: 10,
+      '&:hover': {
+        cursor: 'pointer',
+        boxShadow: '3px 4px 4px 5px gray',
+      },
+      '&:active': {
+        cursor: 'pointer',
+        boxShadow: `1px 2px 2px 5px ${palette.grey[700]}`,
+      }
     },
     postTitle: {
       color: '#61dafb'
