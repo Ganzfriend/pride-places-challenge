67f5813ed40d8610b8f6295967f09563e8585c27
Ganzfriend <melissaganzfried@gmail.com>
2021-10-19 12:31:47 +0200

    updates

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,9 +1,9 @@
-import React, {useState, useEffect} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import faker from 'faker';
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import faker from "faker";
 
-import PostModal from './PostModal.js';
-import styles from '../styles/styles.js';
+import PostModal from "./PostModal.js";
+import styles from "../styles/styles.js";
 
 const useStyles = makeStyles(styles);
 
@@ -19,18 +19,18 @@ const Posts = () => {
   const handleClose = () => setModalPost(null);
 
   const getData = () => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
+    fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
       .then((json) => setPosts(json));
   };
 
   const getUsers = () => {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((json) => {
-         // the linter isn't liking the object literal spread operator for some reason
+        // the linter isn't liking the object literal spread operator for some reason
         // const newUsers = [...json].map(u => {...u, avatar: faker.internet.avatar()});
-        const newUsers = [...json].map(u => {
+        const newUsers = [...json].map((u) => {
           u.avatar = faker.internet.avatar();
           return u;
         });
@@ -39,7 +39,11 @@ const Posts = () => {
       .catch((err) => console.log(err));
   };
 
-  const matchUser = (_id) => users.find(({id}) => id === _id) || users.find(({id}) => id === Math.ceil(Math.random() * 10));
+  // I added a conditional to matchUser to find some random user data if, for some reason, it's passed an invalid user id (an id not present in the users array)
+  // this is an edge case that would cause errors, so I provided a (not real world) solution to prevent the error by defaulting to other dummy data
+  const matchUser = (_id) =>
+    users.find(({ id }) => id === _id) ||
+    users.find(({ id }) => id === Math.ceil(Math.random() * 10));
 
   useEffect(() => {
     getData();
@@ -48,26 +52,29 @@ const Posts = () => {
 
   return (
     <div className={classes.posts}>
-      {!!users.length && posts?.map(({body, id, title, userId}) => {
-        // I was asked to provide the post author's catchPhrase, but what I'm seeing in the data provided by this API, it's not the author's catchPhrase, but rather the company's
-        // If this isn't the catchPhrase you were looking for, I'd be happy to go back and fix this error, or even provide alternate dummy data for the author's catchPhrase! Just let me know :)
-        const {name, company, avatar} = matchUser(userId);
-        const {catchPhrase} = company;
-        const postInfo = {name, catchPhrase, avatar, body, title};
-        return (
-          <div key={id} className={classes.post}>
-            <div onClick={() => handleClickPost(postInfo)}>
-              <h5 className={classes.postTitle}>
-                  {title}
-              </h5>
-              <h6>{body}</h6>
+      {!!users.length &&
+        posts?.map(({ body, id, title, userId }) => {
+          // I was asked to provide the post author's catchPhrase, but what I'm seeing in the data provided by this API, it's not the author's catchPhrase, but rather the company's
+          // If this isn't the catchPhrase you were looking for, I'd be happy to go back and fix this error, or even provide alternate dummy data for the author's catchPhrase! Just let me know :)
+          const { name, company, avatar } = matchUser(userId);
+          const { catchPhrase } = company;
+          const postInfo = { name, catchPhrase, avatar, body, title };
+          return (
+            <div key={id} className={classes.post}>
+              <div onClick={() => handleClickPost(postInfo)}>
+                <h5 className={classes.postTitle}>{title}</h5>
+                <h6>{body}</h6>
+              </div>
             </div>
-          </div>
-        )
-      })}
-      {modalPost &&
-        <PostModal open={!!modalPost} handleClose={handleClose} modalPost={modalPost} />
-      }
+          );
+        })}
+      {modalPost && (
+        <PostModal
+          open={!!modalPost}
+          handleClose={handleClose}
+          modalPost={modalPost}
+        />
+      )}
     </div>
   );
 };
